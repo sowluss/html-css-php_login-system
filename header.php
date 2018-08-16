51a3d677c90ecf15204ca2a98b06c74becc5f3c2
sowluss <sowluss@gmail.com>
2018-08-17 01:47:01 +0300

    added index.php, header.php, signup.php, footer.php, style.css while practicing php login system

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login System</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <!--  google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Halant" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Palanquin" rel="stylesheet">
+</head>
+<body>
+    
+<header>
+    <nav>
+        <div class="main-wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+            </ul>
+            <div class="nav-login">
+                <form action="">
+                    <input type="text" name="uid" placeholder="Username/E-mail">
+                    <input type="password" name="pwd" placeholder="Password" autocomplete="new-password">
+                    <button type="submit" name="submit">Login</button>
+                </form>
+                <a href="signup.php">Sign Up</a>
+            </div>
+ 
+        </div>
+    </nav>
+</header>
---
+++ signup.php
@@ -0,0 +1,23 @@
+<?php
+    include_once 'header.php';
+?>
+
+<section class="main-container">
+<div class="main-wrapper">
+    <h2>Signup </h2>
+    <form class="signup-form">
+        <input type="text" name="first" placeholder="First Name">
+        <input type="text" name="last" placeholder="Last Name">
+        <input type="text" name="email" placeholder="Email Address">
+        <input type="text" name="uid" placeholder="Username">
+        <input type="password " name="pwd" placeholder="Password">
+        <button type="submit" name="submit">Sign Up</button>
+    </form>
+</div>
+</section>
+
+<?php
+    include_once 'footer.php';
+?> 
+
+  
