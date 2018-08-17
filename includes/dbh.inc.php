3a1f3f58cce3b3d9df246e6c62bbb7f104c31cf3
sowluss <sowluss@gmail.com>
2018-08-17 23:54:20 +0300

    modified signup.php and added dbh.inc.php and signup.inc.php files

---
+++ includes/dbh.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "localhost"; 
+$dbUsername = "root"; 
+$dbPassword = "root"; 
+$dbName = "loginsystem";  
+
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+ 
